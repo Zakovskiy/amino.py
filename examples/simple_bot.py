1a8c3c8ea230f3165fc91a3658ad1f58a50c6d6e
zak <60068872+Zakovskiy@users.noreply.github.com>
2021-02-05 18:11:54 +0300

    Update simple_bot.py

--- examples/simple_bot.py
+++ examples/simple_bot.py
@@ -5,17 +5,13 @@ from amino import Client
 
 Amino = Client(email="email", password="password");
 
-while True:
-	try:
-		result = Amino.listen();
-	except:
-		continue;
+while 1:
+	result = Amino.listen()
+	
+	mtype = result["message_type"];
+	community_id = result["community_id"];
+	thread_id = result["thread_id"];
 
-	if(result["t"] == 1000):
-		mtype = result["o"]["chatMessage"]["type"];
-		community_id = str(result["o"]["ndcId"]);
-		thread_id = result["o"]["chatMessage"]["threadId"];
-
-		if(mtype == 0):
-			msg = result["o"]["chatMessage"]["content"];
-			Amino.send(message=msg, community_id=community_id, thread_id=thread_id);
+	if(mtype == 0):
+		msg = result["content"];
+		Amino.send(message=msg, community_id=community_id, thread_id=thread_id);
