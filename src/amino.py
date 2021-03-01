21658709269cb12133206edae1daf8229df7ce15
zak <60068872+Zakovskiy@users.noreply.github.com>
2021-03-01 23:40:16 +0300

    Update amino.py

--- src/amino.py
+++ src/amino.py
@@ -14,7 +14,7 @@ from locale import getdefaultlocale as locale
 
 class Client:
 
-	def __init__(self, email, password, deivce_id:str="01D4651E9A8F92763D2DEC4D62F1CAC312B9784014AC7ED24346EF8132D6D286F8A2087F4C320CE82B"):
+	def __init__(self, email:str=None, password:str=None, deivce_id:str="01D4651E9A8F92763D2DEC4D62F1CAC312B9784014AC7ED24346EF8132D6D286F8A2087F4C320CE82B"):
 		self.api = "https://service.narvii.com/api/v1/";
 		self.headers = {
 			"NDCDEVICEID": deivce_id,
@@ -30,6 +30,36 @@ class Client:
 			self.sid = "?";
 		else:
 			self.authorization(email, password);
+
+	def announcement(self):
+		return requests.get(f"{self.api}g/s/announcement?language=ru&start=0&size=1&sid={self.sid}", headers=self.headers).json();
+
+	def account(self):
+		return requests.get(f"{self.api}g/s/account?sid={self.sid}", headers=self.headers).json();
+
+	def follow(self, community_id:int=0, userId:str=None):
+		return requests.post(f"{self.api}/x{community_id}/s/user-profile/{userId}/member?sid={self.sid}", headers=self.headers).json();
+
+	def register(self, nickname: str = None, email: str = None, password: str = None, deviceId: str = None):
+		if not deviceId:
+			deviceId = self.headers["NDCDEVICEID"];
+
+		data = json.dumps({
+			"secret": f"0 {password}",
+			"deviceID": deviceId,
+			"email": email,
+			"clientType": 100,
+			"nickname": nickname,
+			"latitude": 0,
+			"longitude": 0,
+			"address": None,
+			"clientCallbackURL": "narviiapp://relogin",
+			"type": 1,
+			"identity": email,
+			"timestamp": int(time.time() * 1000)
+		})
+
+		return requests.post(f"{self.api}g/s/auth/register", data=data, headers=self.headers).json();
 
 	def authorization(self, email, password):
 		result = requests.post(f"{self.api}g/s/auth/login", data=json.dumps({"email":email,"secret":"0 "+password,"deviceID":self.headers["NDCDEVICEID"],"clientType":100,"action":"normal","timestamp":(int(time.time() * 1000))}), headers=self.headers)
@@ -56,8 +86,7 @@ class Client:
 		return response.json()
 
 	def get_notification(self, community_id, start:int = 0, size:int = 10):
-		response = requests.get(f"{self.api}/x{community_id}/s/notification?start={start}&size={size}&sid="+self.sid, headers=self.headers);
-		return response.json();
+		return requests.get(f"{self.api}/x{community_id}/s/notification?start={start}&size={size}&sid="+self.sid, headers=self.headers).json();
 
 	def check_device(self, deviceId: str):
 		data = json.dumps({
@@ -74,24 +103,19 @@ class Client:
 		return response.json()
 
 	def get_wallet_info(self):
-		response = requests.get(f"{self.api}/g/s/wallet?sid="+self.sid, headers=self.headers).json();
-		return response;
+		return requests.get(f"{self.api}/g/s/wallet?sid="+self.sid, headers=self.headers).json();
 
 	def get_wallet_history(self, start:int = 0, size:int = 25):
-		response = requests.get(f"{self.api}/g/s/wallet/coin/history?start={start}&size={size}&sid="+self.sid, headers=self.headers)
-		return response.json();
+		return requests.get(f"{self.api}/g/s/wallet/coin/history?start={start}&size={size}&sid="+self.sid, headers=self.headers).json();
 
 	def moderation_history_user(self, community_id:int = 0, userId: str = None, size: int = 25):
-		response = requests.get(f"{self.api}x{community_id}/s/admin/operation?pagingType=t&size={size}&sid={self.sid}", headers=self.headers)
-		return response.json();
+		return requests.get(f"{self.api}x{community_id}/s/admin/operation?pagingType=t&size={size}&sid={self.sid}", headers=self.headers).json();
 
 	def get_comms(self, start: int = 0, size: int = 25):
-		response = requests.get(f"{self.api}g/s/community/joined?start={start}&size={size}&sid="+self.sid, headers=self.headers).json()
-		return response
+		return requests.get(f"{self.api}g/s/community/joined?start={start}&size={size}&sid="+self.sid, headers=self.headers).json();
 
 	def watch_ad(self):
-		response = requests.post(f"{self.api}g/s/wallet/ads/video/start?sid={self.sid}", headers=self.headers)
-		return response.json();
+		return requests.post(f"{self.api}g/s/wallet/ads/video/start?sid={self.sid}", headers=self.headers).json();
 
 	def transfer_host(self, community_id, chatId: str, userIds: list):
 		data = json.dumps({
@@ -103,16 +127,13 @@ class Client:
 		return response;
 
 	def join_chat(self, community_id, thread_id):
-		response = requests.post(f"{self.api}x{community_id}/s/chat/thread/{chat_id}/member/{self.auid}?sid="+self.sid, headers=self.headers).json()
-		return response;
+		return requests.post(f"{self.api}x{community_id}/s/chat/thread/{chat_id}/member/{self.auid}?sid="+self.sid, headers=self.headers).json();
 
 	def getMessages(self, community_id, thread_id, size):
-		res = requests.get(f"{self.api}x{community_id}/s/chat/thread/{thread_id}/message?v=2&pagingType=t&size={size}&sid="+self.sid, headers=self.headers)
-		return res.json()
+		return requests.get(f"{self.api}x{community_id}/s/chat/thread/{thread_id}/message?v=2&pagingType=t&size={size}&sid="+self.sid, headers=self.headers).json()
 
 	def getThread(self, community_id, thread_id):
-		res = requests.get(f"{self.api}x{community_id}/s/chat/thread/{thread_id}?sid="+self.sid, headers=self.headers);
-		return res.json();
+		return requests.get(f"{self.api}x{community_id}/s/chat/thread/{thread_id}?sid="+self.sid, headers=self.headers).json();
 
 	def sendAudio(self, path, community_id, thread_id):
 		audio = base64.b64encode(open(path, "rb").read())
@@ -130,8 +151,7 @@ class Client:
 		return response;
 
 	def get_banned_users(self, community_id, start: int = 0, size: int = 25):
-		response = requests.get(f"{self.api}x{community_id}/s/user-profile?type=banned&start={start}&size={size}&sid="+self.sid, headers=self.headers)
-		return response.json();
+		return requests.get(f"{self.api}x{community_id}/s/user-profile?type=banned&start={start}&size={size}&sid="+self.sid, headers=self.headers).json();
 
 	def unban(self, community_id, userId: str, reason: str):
 		data = json.dumps({
@@ -141,8 +161,7 @@ class Client:
 			"timestamp": int(time.time() * 1000)
 		})
 
-		response = requests.post(f"{self.api}x{community_id}/s/user-profile/{userId}/unban?sid={self.sid}", headers=self.headers, data=data)
-		return response.json();
+		return requests.post(f"{self.api}x{community_id}/s/user-profile/{userId}/unban?sid={self.sid}", headers=self.headers, data=data).json();
 
 		
 
@@ -328,7 +347,7 @@ class Client:
 		return response.json()
 
 	def get_users_community(self, community_id, start:int = 0, size:int = 25):
-		response = requests.get(f"{self.api}x{community_id}/s/user-profile?type=recent&start={start}&size={size}&sid="+self.sid).json();
+		response = requests.get(f"{self.api}x{community_id}/s/user-profile?type=recent&start={start}&size={size}&sid="+self.sid, headers=self.headers).json();
 		return response
 
 	def leave_chat(self, community_id, thread_id):
