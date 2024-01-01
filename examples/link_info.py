ddc2ff2bf6b42c6ab7552f268a0311592b0c81c5
zak <60068872+Zakovskiy@users.noreply.github.com>
2024-01-01 15:43:40 +0300

    Update link_info.py

--- examples/link_info.py
+++ examples/link_info.py
@@ -1,16 +1,13 @@
-# /Zakovskiy/amino.py/
-# get info by thread link
-
 from amino import Client
 
 Amino = Client("email", "password");
 
-link 		   = input("Link on thread: ");
+link = input("Link on thread: ");
 
-info 		   = Amino.get_from_code(link)["linkInfoV2"]["extensions"]["linkInfo"];
+info = Amino.get_from_code(link)["linkInfoV2"]["extensions"]["linkInfo"];
 
-community_id   = info["ndcId"];
-thread_id      = info["objectId"];
+community_id = info["ndcId"];
+thread_id = info["objectId"];
 
 print(f"Community id: {community_id}");
 print(f"Thread id: {thread_id}");
